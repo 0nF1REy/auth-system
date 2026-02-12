05862cb38b620986d0ea63bcfcc0af8b06c5d4b2
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-02-11 22:45:41 -0300

    T001: feat (web/services) - Implemented Auth service with user registration and login methods
    - Introduced a Auth service for user authentication, including registration and login functionality, along with initial unit tests.

---
+++ web/src/app/services/auth.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Usuario {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private apiUrl = 'http://localhost:8080/api/usuarios';
+  constructor(private http: HttpClient) {}
+
+  cadastrar(usuario: Usuario): Observable<string> {
+    return this.http.post(`${this.apiUrl}/cadastro`, usuario, {
+      responseType: 'text',
+    });
+  }
+  login(usuario: Usuario): Observable<Usuario | null> {
+    return this.http.post<Usuario | null>(`${this.apiUrl}/login`, usuario);
+  }
+}
